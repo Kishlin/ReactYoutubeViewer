b5853dcd3b33067fa4e2f4b93c32e65f78537c9b
Kishlin <legrand.pierrelouis@gmail.com>
2017-06-28 21:59:40 +0200

    Event handling in search bad.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import SearchBar from './search_bar';
+
 export default class App extends Component {
   render() {
     return (
-      <div>Hello world</div>
+      <SearchBar />
     );
   }
 }

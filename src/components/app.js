4dbc0a73418d72c215cc8b369024e2ea3c7e504c
Kishlin <legrand.pierrelouis@gmail.com>
2017-06-30 15:14:59 +0200

    Youtube API

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+import Constants from '../constants/constants';
 
 import SearchBar from './search_bar';
 
 export default class App extends Component {
-  render() {
-    return (
-      <SearchBar />
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: []
+		};
+	}
+
+	componentDidMount() {
+		YTSearch({key: Constants.API_KEY, term: 'surfboards'}, videos => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		console.log(this.state);
+
+		return (
+			<SearchBar />
+		);
+	}
 }
